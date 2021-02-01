5ff4be2a92ab41b3ac4782c356a71eab2b462d3b
ArtemShakun <artem.shakun.91@gmail.com>
2021-02-01 20:56:36 +0200

    01/02/21TasksJavaScript

---
+++ JavaScript/01.02.21/js/script.js
@@ -0,0 +1,58 @@
+// TASK--1 Посчитаем сумму чисел от 1 до n
+
+// const n = 167;
+// let sum = 0;
+
+// for (let i = 0; i <= n; i++) {
+//    sum += i
+// }
+
+// console.log('Result: ' + sum);
+
+// TASK--2 Посчитать произведение всех нечетных чисел от m до n
+// const m = 55;
+// const n = 77;
+// let result = 1;
+
+// let i = m;
+
+// do {
+//     if (i % 2 === 1) {
+//         result *= i;
+//     }
+//     i++;
+// } while (i <= n);
+
+// console.log('Result: ' + result);
+
+//  TASK--3 Выведите в консоль таблицу умножения от 1 до 5
+// for (let i = 1; i <= 5; i++) {
+//    console.log('\n=> ' + i + ' <=\n\n');
+
+//    for (let j = 1; j <= 10; j++) {
+//        console.log(i + ' x ' + j + ' = ' + i * j);
+//    }
+// }
+
+// TASK--4 Создайте ф-цию, которая складывает два числа
+// function sum(a, b) {
+//    return a + b;
+// };
+
+// Task--5 Создайте ф-цию, которая выводит в консоль приветствие
+// function sayHi(name) {
+//    const greeting = 'Hi, ' + name;
+//    console.log(greeting);
+// }
+
+// TASK--6 Создайте ф-цию, которая определяет парность числа
+// function getEvenOdd(num) {
+//    if (num % 2 === 1) {
+//        return 'odd';
+//    }
+
+//    return 'even';
+// }
+
+// TASK--7 Стрелочная ф-ция возводящая число в квадрат
+// let square = num => num * num;
