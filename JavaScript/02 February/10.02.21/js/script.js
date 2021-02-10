9f7152d0bc9ec949bee3cd217e867e0cbf96f06a
ArtemShakun <artem.shakun.91@gmail.com>
2021-02-10 21:17:51 +0200

    10/02 global obj

--- JavaScript/02 February/10.02.21/js/script.js
+++ JavaScript/02 February/10.02.21/js/script.js
@@ -9,31 +9,43 @@
 // }
 
 
-function getPerson(obj) {
-   console.log('Print object: ', obj)
-   for (let key in this) {
-      if (this.hasOwnProperty(key)) {
-         console.log('[' + key + ']', this[key])
-      }
+// function getPerson(obj) {
+//    console.log('Print object: ', obj)
+//    for (let key in this) {
+//       if (this.hasOwnProperty(key)) {
+//          console.log('[' + key + ']', this[key])
+//       }
+//    }
+// }
+
+// const person = {
+//    name: 'Artem',
+//    age: 29,
+// }
+
+// const car = {
+//    name: 'Ford',
+//    model: 'Focus',
+//    year: 2015,
+// }
+
+// // То что мы передаюм в метод bind является котекстом
+// let printPerson = getPerson.bind(person)
+// printPerson('person')
+// // метод call делает то что и bind только вызывает функцию сразу. 
+// // Параметры указываються через запятую, бузконечное к-во
+// getPerson.call(car, 'Car')
+// getPerson.apply(person, ['Person'])
+// // метод aplly делает то что и call только параметры передаются в массиве.
+const wallet = {
+   transactions: [1, 5, 87, 198, 3],
+   getMax() {
+      return Math.max(...this.transactions)// современный способо
+   },
+   getMin() {
+      return Math.min.apply(null, this.transactions) // старый способо
    }
-}
+};
 
-const person = {
-   name: 'Artem',
-   age: 29,
-}
-
-const car = {
-   name: 'Ford',
-   model: 'Focus',
-   year: 2015,
-}
-
-// То что мы передаюм в метод bind является котекстом
-let printPerson = getPerson.bind(person)
-printPerson('person')
-// метод call делает то что и bind только вызывает функцию сразу. 
-// Параметры указываються через запятую, бузконечное к-во
-getPerson.call(car, 'Car')
-getPerson.apply(person, ['Person'])
-// метод aplly делает то что и call только параметры передаются в массиве.
+console.log(wallet.getMax())
+console.log(wallet.getMin())
