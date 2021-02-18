bceab60b9dd72ed3b62dc1dd0e32dbf4757eaa54
ArtemShakun <artem.shakun.91@gmail.com>
2021-02-18 08:34:59 +0200

    18/02/21 Recursion

--- JavaScript/02 February/18.02.21/js/script.js
+++ JavaScript/02 February/18.02.21/js/script.js
@@ -49,3 +49,43 @@
 
 
 // РЕКУРСИЯ
+
+// const favorites = ['id-6', 'id-17'];
+
+// const tree = {
+//    id: 'id-1',
+//    name: 'Products',
+
+//    nodes: [
+//       {
+//          id: 'id-2',
+//          name: 'food',
+//          nodes: [
+//             {
+//                id: 'id-6',
+//                name: 'Drinks',
+//                nodes: [],
+//             }
+//          ]
+//       },
+//       {
+//          id: 'id-17',
+//          name: 'Vehicles',
+//          nodes: []
+//       }
+//    ],
+// };
+
+// const markFavorites = (tree, favorites) => {
+//    const isFavorite = favorites.includes(tree.id);
+
+//    return {
+//       ...tree,
+//       isFavorite,
+//       nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
+//    }
+// }
+// const result = markFavorites(tree, favorites);
+// console.log(result)
+
+// КАРРИРОВАНИЕ
